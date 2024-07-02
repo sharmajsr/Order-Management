6d7832085253c203e47a5d7927bf3609d0115150
sharmajsr <shubhamsharma1414@gmail.com>
2024-07-02 06:51:07 +0530

    Added constants file for ref

---
+++ common/constants.go
@@ -0,0 +1,8 @@
+package common
+
+const CFG_KEY_DB_SCHEMA_PATH = "/Users/shubhamsharma/Projects/Order-Management/db/schema/"
+const CFG_KEY_PG_NAME = "postgres"
+const CFG_KEY_PG_PWD = "postgres"
+const CFG_KEY_PG_USER = "postgres"
+const CFG_KEY_PG_HOST = "localhost"
+const CFG_KEY_PG_PORT = "5432"
